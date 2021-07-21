25c8ac9e0ef1fdff796f6dfab19253a66217d9a9
Vaansh Sanghrajka <vaanshsanghrajka9@gmail.com>
2021-07-21 22:28:47 +0530

    first commit

---
+++ Arithmeticojs.js
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+	<script type="text/javascript">
+
+		var num;
+		var num1;
+	
+		num=parseInt(prompt("Enter Num: ");
+		num1=parseInt(prompt("Enter Num1: );
+		
+		document.write("<br><br>Arithmetic Operator<br><br>");
+	
+		document.write("+ is" +(num+num1));
+		document.write("<br>");
+		document.write("* is" +(num*num1));
+		document.write("<br>");
+		document.write("- is" +(num-num1));
+		document.write("<br>");
+		document.write("/ is" +(num/num1));
+		document.write("<br>");
+		document.write("% is" +(num%num1));
+		document.write("<br>");
+	</body>
+	</script>
+</html>
